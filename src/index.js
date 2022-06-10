53b8210881096fddbcb75e603fc1294ffc661b6e
srinivasaraodannana <saikiran.dannana@gmail.com>
2022-06-10 22:29:57 +0530

    tested 4 asset module types

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import helloWorld from './hello-world';
+import book from './java_book.jpg';
+import altText from './alt_name.txt';
+
 console.log('Sample Webpack Config');
 helloWorld();
+
+const img=document.createElement('img');
+img.width='200';
+img.src=book;
+img.alt=altText;
+
+const body=document.querySelector('body');
+body.appendChild(img);
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,24 @@ module.exports={
   output:{
     filename:'bundle.js',
     path:path.resolve(__dirname, './dist'),
+    publicPath:'dist/'
   },
-  mode:'production',
+  mode:'development',
+  module:{
+    rules:[
+      {
+        test:/\.(jpg|png)$/,
+        type:'asset',
+        parser:{
+          dataUrlCondition:{
+            maxSize: 8*1024 // 8 kb
+          }
+        }
+      },
+      {
+        test:/\.txt/,
+        type:'asset/source'
+      }
+    ]
+  }
 }
